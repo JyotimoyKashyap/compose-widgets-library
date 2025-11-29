07ba0fe64b2ee2052b193e386c3375d51fda404b
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2025-11-29 16:02:42 +0530

    feat: Remove unused imports
    Removed an unused import statement for `androidx.compose.animation.core.*` from `RipplingDotGrid.kt`.

--- composecomponents/src/main/java/com/jyotimoykashyap/composecomponents/RipplingDotGrid.kt
+++ composecomponents/src/main/java/com/jyotimoykashyap/composecomponents/RipplingDotGrid.kt
@@ -1,6 +1,5 @@
 package com.jyotimoykashyap.composecomponents
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
